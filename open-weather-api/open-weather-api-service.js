2a30ed58d30be6277862555af71d276e14485a5f
Adrian Nasir Fauske <adrianfauske@gmail.com>
2023-03-16 19:45:50 +0100

    Finalize async await, finalize weather.js

--- open-weather-api/open-weather-api-service.js
+++ open-weather-api/open-weather-api-service.js
@@ -9,67 +9,47 @@ class OpenWeatherAPIService {
     // The API key that is needed to successfully authenticate when calling the Open Weather API. 
     // if this does not work, register one yourself using the instruction here: https://openweathermap.org/appid
     static apiKey = "c8da4ee199183df66c17fe7a2b6bf6da";
-    static lat = 0.0;
-    static lon = 0.0;
 
     /**
      * Retrieves the weather data for the given city by calling the Open Weather API.
     */
     async getWeather(cityName) {
-        //TODO: Implement...
-        let toBreak = false;
-        axios.get(
-          `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=c8da4ee199183df66c17fe7a2b6bf6da`
-        )
-        .then(cityJson => {
-            console.log("\nCITYJSON\n");
-            console.log(cityJson);
-            console.log("\nCITYJSON_END\n");
-            this.lat = cityJson.data.lat;
-            this.lon = cityJson.data.lon;
-        })
-        .catch(function (error){
-            console.log(error);
-            console.log("\n\nERROR IN GETCITY!!!\n\n");
-            toBreak = true;
-        })
-        .finally(function () {
-            //always executed
-            console.log("Attempted to collect latitude and longtitude data");
-        });
-        console.log("\nChecking toBreak value" + toBreak + "\n\n");
-        if (toBreak == true) {
-          console.log("perform toBreak");
-          return;
+      const getWeatherData = await axios.get(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${OpenWeatherAPIService.apiKey}`
+      )
+      .then(response => {
+        console.log("\nCITYJSON\n");
+        console.log(response.data);
+        console.log("\nCITYJSON_END\n");
+        var [lat, lon] = [response.data[0].lat, response.data[0].lon];
+        console.log("Getting city at lat: " + lat + " lon: " + lon);
+        return axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${OpenWeatherAPIService.apiKey}&units=metric`);
+      })
+      .then(response => {
+        console.log("\nWEATHERJSON\n");
+        console.log(response.data);
+        console.log("\nWEATHERJSON_END\n");
+        var returnWeatherObject = {};
+        returnWeatherObject["cityName"] = cityName;
+        returnWeatherObject["temperature"] = response.data.main.temp_min;
+        returnWeatherObject["weatherDescription"] = response.data.weather[0].description;
+        return returnWeatherObject;
+      })
+      .catch(function (error) {
+        console.log("\n\n!!! AXIOS GET ERROR !!!\n\n")
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          console.log(error.request);
+        } else {
+          console.log("Error: " + error.message);
         }
-
-        axios
-          .get(
-            `https://api.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lon}&appid=c8da4ee199183df66c17fe7a2b6bf6da`
-          )
-          .then(weatherJson => {
-            console.log("\nWEATHERJSON\n");
-            console.log(weatherJson);
-            console.log("\nWEATHERJSON_END\n");
-            const weatherObj = JSON.parse(weatherJson);
-            const returnWeatherObj = {};
-            returnWeatherObj.cityName = cityName;
-            returnWeatherObj.temperature = weatherObj.data.main.temp_min - 273.15; //Conversion from Kelvin to Celcius
-            returnWeatherObj.weatherDescription = weatherObj.data.weather.description; //Collect description of weather
-          })
-          .catch(function (error) {
-            console.log(error);
-            console.log("\n\nERROR IN GETWEATHER!!!\n\n");
-            return;
-          })
-          .finally(function () {
-            console.log("Attempted to collect weather data");
-          });
-          console.log("exited last get request")
-        return await this.returnWeatherObj;
-        // If valid weather data is found, return JSON object. Else return Null object
-        // Check for if returnWeatherObejct === undefined and throw 404 if true
-        // To upload to web server, should probably do JSON.stringify(returnWeatherobj);
+        console.log(error.config);
+        return;
+      });
+      return getWeatherData;
     }
 }
 
